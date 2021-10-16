0929566e06202c0120ff0a5976327e065d66c863
NAME <mail@gmail.com>
2021-10-16 12:45:43 +0900

    Plugin error fix

--- src/handlers/MessageHandler.js
+++ src/handlers/MessageHandler.js
@@ -2,12 +2,14 @@ const MessageHandler = function(msg) {
     const config = JSON.parse(require('fs').readFileSync("kommando_config.json"));
     const plugins = Object.values(config.plugins);
     plugins.forEach(plugin => {
-        try {
+        if (!plugin instanceof Boolean) {
+            try {
             if (plugin.perms.find(perm => perm === "msg")) require(`../../../../node_modules/${plugin}`).emit("messageCreate", msg);
-        } catch(err) {
+            } catch(err) {
             console.error(err);
             msg.channel.send(config.messages.ERROR);
             require('./ErrorHandler.js')(err, msg.client);
+            }
         }
     });
 }
